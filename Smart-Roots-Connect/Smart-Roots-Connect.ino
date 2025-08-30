b408d16f49ace66bd7a33ff826a28f7e54315755
ShravanRamjathan <142317289+ShravanRamjathan@users.noreply.github.com>
2025-08-30 20:10:45 +0200

    Update Smart-Roots-Connect.ino

--- Smart-Roots-Connect/Smart-Roots-Connect.ino
+++ Smart-Roots-Connect/Smart-Roots-Connect.ino
@@ -1,131 +1,107 @@
-const char* ssid = "Campus specific wifi";
-const char* password = "the password";
-#define channelId  3036727
-const char* mqtt_username = "DwQyEDY9LRAoARswJiYeEwY";
-const char* ClientId = "DwQyEDY9LRAoARswJiYeEwY";
-const char* mqttPass="laDzQ7nAwxSX/xZftsx/DeEi";
+/*
+ Basic MQTT example
+
+ This sketch demonstrates the basic capabilities of the library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic"
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ 
+*/
+
+#include <SPI.h>
 #include <PubSubClient.h>
+#include <WiFiClientSecure.h>
 #include <WiFi.h>
- const char * PROGMEM thingspeak_ca_cert = \
-  "-----BEGIN CERTIFICATE-----\n" \
-  "MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBs\n" \
-  "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
-  "d3cuZGlnaWNlcnQuY29tMSswKQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5j\n" \
-  "ZSBFViBSb290IENBMB4XDTA2MTExMDAwMDAwMFoXDTMxMTExMDAwMDAwMFowbDEL\n" \
-  "MAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3\n" \
-  "LmRpZ2ljZXJ0LmNvbTErMCkGA1UEAxMiRGlnaUNlcnQgSGlnaCBBc3N1cmFuY2Ug\n" \
-  "RVYgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMbM5XPm\n" \
-  "+9S75S0tMqbf5YE/yc0lSbZxKsPVlDRnogocsF9ppkCxxLeyj9CYpKlBWTrT3JTW\n" \
-  "PNt0OKRKzE0lgvdKpVMSOO7zSW1xkX5jtqumX8OkhPhPYlG++MXs2ziS4wblCJEM\n" \
-  "xChBVfvLWokVfnHoNb9Ncgk9vjo4UFt3MRuNs8ckRZqnrG0AFFoEt7oT61EKmEFB\n" \
-  "Ik5lYYeBQVCmeVyJ3hlKV9Uu5l0cUyx+mM0aBhakaHPQNAQTXKFx01p8VdteZOE3\n" \
-  "hzBWBOURtCmAEvF5OYiiAhF8J2a3iLd48soKqDirCmTCv2ZdlYTBoSUeh10aUAsg\n" \
-  "EsxBu24LUTi4S8sCAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQF\n" \
-  "MAMBAf8wHQYDVR0OBBYEFLE+w2kD+L9HAdSYJhoIAu9jZCvDMB8GA1UdIwQYMBaA\n" \
-  "FLE+w2kD+L9HAdSYJhoIAu9jZCvDMA0GCSqGSIb3DQEBBQUAA4IBAQAcGgaX3Nec\n" \
-  "nzyIZgYIVyHbIUf4KmeqvxgydkAQV8GK83rZEWWONfqe/EW1ntlMMUu4kehDLI6z\n" \
-  "eM7b41N5cdblIZQB2lWHmiRk9opmzN6cN82oNLFpmyPInngiK3BD41VHMWEZ71jF\n" \
-  "hS9OMPagMRYjyOfiZRYzy78aG6A9+MpeizGLYAiJLQwGXFK3xPkKmNEVX58Svnw2\n" \
-  "Yzi9RKR/5CYrCsSXaQ3pjOLAEFe4yHYSkVXySGnYvCoCWw9E1CAx2/S6cCZdkGCe\n" \
-  "vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep\n" \
-  "+OkuE6N36B9K\n" \
-  "-----END CERTIFICATE-----\n";
-#define mqttPort 1883
-WiFiClient client;
-const char* server = "mqtt3.thingspeak.com";
-int status = WL_IDLE_STATUS; 
-long lastPublishMillis = 0;
-int connectionDelay = 1;
-int updateInterval = 15;
-PubSubClient mqttClient( client );
+// Update these with values suitable for your network.
 
-void setup() {
-  // put your setup code here, to run once:
-Serial.begin(115200);
-delay(3000);
-connectWifi();
-//configure the client
-mqttClient.setServer(server, mqttPort);
-//set the mqtt message handler
-mqttClient.setCallback(mqttSubscribtionCallback);
- // Set the buffer to handle the returned JSON. NOTE: A buffer overflow of the message buffer will result in your callback not being invoked.
- mqttClient.setBufferSize(2048);
- client.setCACert(thingspeak_ca_cert);
-}
+char ssid[] = "wifi name";
+char password[] = "wifi_pass";
+const char *mqtt_broker = "your instance or public broker";
+const char *topic = "Readings";
+const char *mqtt_username = "your_username";
+const char *mqtt_password = "your_password";
+const int mqtt_port = 8883;
 
-void loop() {
-  // put your main code here, to run repeatedly:
-   // Reconnect to WiFi if it gets disconnected.
-  if (WiFi.status() != WL_CONNECTED) {
-      connectWifi();
-  }
-  
-  // Connect if MQTT client is not connected and resubscribe to channel updates.
-  if (!mqttClient.connected()) {
-     mqttConnect(); 
-     mqttSubscribe( channelID );
-  }
-  
-  // Call the loop to maintain connection to the server.
-  mqttClient.loop(); 
-  
-  // Update ThingSpeak channel periodically. The update results in the message to the subscriber.
-  if ( abs(long(millis()) - lastPublishMillis) > updateInterval*1000) {
-    mqttPublish( channelID, (String("field1=")+String(WiFi.RSSI())) );
-    lastPublishMillis = millis();
-  }
-}
-
-
-
-// Function to handle messages from MQTT subscription.
-void mqttSubscriptionCallback( char* topic, byte* payload, unsigned int length ) {
-  // Print the details of the message that was received to the serial monitor.
+const char* ca_cert= \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+"QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+"MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+"b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+"9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+"CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+"nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+"43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+"T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+"gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+"BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+"TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+"DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+"hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+"06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+"PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+"YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+"CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=" \
+"-----END CERTIFICATE-----\n";
+WiFiClientSecure espClient;
+void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
+  for (int i=0;i<length;i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
 }
 
-void mqttSubscribe( long subChannelID ){
-  String myTopic = "channels/"+String( subChannelID )+"/subscribe";
-  mqttClient.subscribe(myTopic.c_str());
-}
+PubSubClient client(espClient); // add tje c;oemt here
 
-
-// Publish messages to a ThingSpeak channel.
-void mqttPublish(long pubChannelID, String message) {
-  String topicString ="channels/" + String( pubChannelID ) + "/publish";
-  mqttClient.publish( topicString.c_str(), message.c_str() );
-}
-
-void connectWifi(){
-  while(WiFi.status()!=WL_CONNECTED){
-    WiFi.begin(ssid, password);
-    delay(connectionDelay*1000);
-    Serial.println(WiFi.status());
-  }
-  Serial.println("Connected to wifi");
-}
-
-void mqttConnect(){
-  while(!mqttClient.connected()){
-    // connect to mqtt broker
-    if(mqttClient.connect(clientId,mqtt_username, mqttPass)){
-      Serial.print("MQTT to");
-      Serial.print(server);
-      Serial.print(" at port ");
-      Serial.print(mqttPort);
-      Serial.print("Success");
-    }else{
-      Serial.print("MQTT connection failed, rc = ");
-        // See https://pubsubclient.knolleary.net/api.html#state for the failure code explanation.
-      Serial.print(mqttClient.state());
-      Serial.println(" Will try again in a few seconds");
-      delay(connectionDelay *1000);
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("Veg-Tent-ESP32", mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("Readings","hello world");
+      // ... and resubscribe
+      client.subscribe("Readings");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
   }
+}
+
+void setup()
+{
+  Serial.begin(57600);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+}
+espClient.setCACert(ca_cert);
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  reconnect();
+  delay(1500);
+}
+
+void loop()
+{
+   
+  client.loop();
 }
